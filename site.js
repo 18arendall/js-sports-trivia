1fe8da420d4f6dfe261af7f95d8bb43055aac877
Adam Rendall <300037469@fvtc.edu>
2026-03-12 17:48:11 -0500

    part 1 done

--- site.js
+++ site.js
@@ -8,14 +8,25 @@ const answersElement = document.querySelector('#answers')
 const nextQuestionElement = document.querySelector('#nextQuestion')
 
 // IIFE (so we can use async/await)
-;(async () => {
+(async () => {
 
 	// todo: create your "getNextQuestion" function
+	const getNextQuestion = async () =>{
 
+		const response = await fetch('https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple')
+		const json = await response.json()
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+
+	}
 	// todo: create your "renderQuestion" function
-
+	const renderQuestion = ({ question, answers, correct }) => {
+		
+	 }
 	// todo: add the event listener to the "nextQuestion" button
 
+	console.log(await getNextQuestion)
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
